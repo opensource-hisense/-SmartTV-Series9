7730056a3728b8e3a828ff0b2cac06334f60a5ea
opensource-hisense <opensource.hisense@gmail.com>
2025-01-17 10:27:05 +0800

    delete oss dir

--- oss/part1/cobalt/cobalt/demos/content/media-element-demo/src/components/source_buffer_info.ts
+++
@@ -1,9 +0,0 @@
-interface Props {
-  sourceBuffer: SourceBuffer;
-  name: string;
-}
-
-/** A component that displays the source buffer info. */
-export function SourceBufferInfo({sourceBuffer, name}: Props) {
-  return `<div>${name} buffered: ${sourceBuffer.buffered.end(0)} sec</div>`;
-}
--- oss/part1/cobalt/cobalt/demos/content/media-element-demo/src/components/video_info.ts
+++
@@ -1,9 +0,0 @@
-interface Props {
-  duration: number;
-  currentTime: number;
-}
-
-/** A component that displays video info. */
-export function VideoInfo({duration, currentTime}: Props) {
-  return `<div>${currentTime} / ${duration}</div>`;
-}
